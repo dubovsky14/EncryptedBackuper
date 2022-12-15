8d26fa69bca4f75d973284fadaa690e334b3679d
Michal Dubovsky <michal.dubovsky@cern.ch>
2022-12-16 00:45:19 +0100

    Moving AESWrapper::xor_with_iv* methods to private part

--- EncryptedBackuper/AESWrapper.h
+++ EncryptedBackuper/AESWrapper.h
@@ -22,9 +22,6 @@ namespace EncryptedBackuper     {
 
             void decrypt(const unsigned char *cipher_text, unsigned char *plain_text);
 
-            void xor_with_iv(unsigned char *text);
-
-            void xor_with_iv_and_store_in_iv(const unsigned char *text);
 
         private:
             unsigned char m_initial_vector[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
@@ -35,5 +32,9 @@ namespace EncryptedBackuper     {
             std::shared_ptr<AES::AESHandler>    m_aes_handler = nullptr;
 
             static std::vector<unsigned char>   cpp_int_to_unsigned_char_vector(const boost::multiprecision::cpp_int &number);
+
+            void xor_with_iv(unsigned char *text);
+
+            void xor_with_iv_and_store_in_iv(const unsigned char *text);
     };
 }
